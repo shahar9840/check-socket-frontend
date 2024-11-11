d79a11778d07f2d0658e6a2ce88b5af5003b8f54
shahar9840 <92571880+shahar9840@users.noreply.github.com>
2024-11-12 01:57:10 +0200

    check socket commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { io } from 'socket.io-client';
+
 
 function App() {
+const socketRef = React.useRef();
+const [checkSocket, setCheckSocket] = React.useState(null);
+
+
+
+React.useEffect(() => {
+  socketRef.current = io('http://localhost:5002');
+  socketRef.current.emit('message', checkSocket);
+  
+  return () => {
+    
+    socketRef.current.disconnect();
+  };
+}, [checkSocket]);
+
+React.useEffect(() => {
+  socketRef.current.on('server_message', (data) => {
+    console.log(data);
+    setCheckSocket(null)
+  })
+}, [checkSocket]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      hello 
+
+      <button onClick={() => {
+        setCheckSocket('send to server message ')}}>click</button>
     </div>
   );
 }
